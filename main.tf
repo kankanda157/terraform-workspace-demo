e817093a1abbc3c2b4bf998ec343c894eb778ac2
kankanda <kankanda157@gmail.com>
2021-12-19 19:19:43 +0800

    commit added the instance type

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
     ami = var.aws_ami_id
-    instance_type = var.aws_instance_id
+    instance_type = "t2.micro"
     count = terraform.workspace == "default" ? 2 : 1
 
     tags = {
